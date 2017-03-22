2763d9f9fbacaacf5e9bf35a50f1d24e185a037c
Kevin Jing Qiu <kevin@idempotent.ca>
2017-03-21 20:47:23 -0400

    Remove terraform

--- terraform/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "site" {
-    bucket = "${var.bucket_name}"
-    acl    = "public-read"
-    website {
-        index_document = "index.html"
-        error_document = "error.html"
-    }
-}
