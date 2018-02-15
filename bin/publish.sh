06e649dcdbe12b558a2af3955d317aafddbfd95e
Kevin Jing Qiu <kevin@idempotent.ca>
2018-02-14 22:20:12 -0500

    Only commit _sites

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,6 +6,7 @@ else
     git config --global user.email kevin@idempotent.ca
     git config --global user.name "Kevin Jing Qiu"
     git stash && git checkout master && git stash pop
-    git commit -am "Publish Site"
+    git add _sites
+    git commit -m "Publish Site"
     git push origin master
 fi
