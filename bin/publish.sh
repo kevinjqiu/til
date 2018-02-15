31e9c1f6a384fa43915172603e112d6cfdf008e8
Kevin Jing Qiu <kevin@idempotent.ca>
2018-02-14 21:22:23 -0500

    Publish Site

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,11 +2,11 @@
 
 
 if [[ -z $(git status -s) ]]; then
+    echo "Nothing to publish"
+else
     git config --global user.email kevin@idempotent.ca
     git config --global user.name "Kevin Jing Qiu"
     git stash && git checkout master && git stash pop
     git commit -am "Publish Site"
     git push origin master
-else
-    echo "Nothing to publish"
 fi
